18c063130f49e8bc896bd946f2a34c349dac0333
Smile App Development <dawsonseibold@gmail.com>
2018-06-15 23:24:42 -0600

    Initial Commit

---
+++ License Plate Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  License Plate Game
+//
+//  Created by Carissa Seibold on 6/15/18.
+//  Copyright © 2018 Smile App Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
